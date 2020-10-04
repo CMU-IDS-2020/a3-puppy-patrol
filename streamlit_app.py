e0a8e5979d2a0b05585c73c6defade93d967f249
Lukas Hermann <lukashermann28@gmail.com>
2020-10-03 22:09:47 -0400

    cleaned rats.csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,28 +2,45 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+st.title("""
+        # Aww Rats!🐀
+        An exploration of rat sightings in New York City, courtesy of [NYC Open Data](https://data.cityofnewyork.us/Social-Services/Rat-Sightings/3q43-55fe).
+        """)
 
 @st.cache  # add caching so we load the data only once
 def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+    filepath = "rats.csv"
+    return pd.read_csv(filepath)
 
 df = load_data()
 
 st.write("Let's look at raw data in the Pandas Data Frame.")
 
-st.write(df)
+#st.write(df)
 
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+#chart = alt.Chart(df).mark_point().encode(
+#    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
+#    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
+#    color=alt.Y("species")
+#).properties(
+#    width=600, height=400
+#).interactive()
+#
+#st.write(chart)
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
-).properties(
-    width=600, height=400
-).interactive()
+#chart = alt.Chart(df, width=800, height=600).mark_circle(size=0.1, opacity=0.25).encode(
+#        longitude="Longitude:Q",
+#        latitude="Latitude:Q",
+#        color="Address Type:N",
+#).transform_filter("datum.Longitude > -74")
+#
+#st.write(chart)
 
-st.write(chart)
+bar = alt.Chart(df).mark_bar().encode(
+        x="Borough:N",
+        y="count(City)"
+)
+
+st.write(bar)
+
+df.columns
