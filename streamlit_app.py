f353d0c8b37e42657f59aa62a3f8d0eeff0b016d
Lukas Hermann <lukashermann28@gmail.com>
2020-10-03 22:48:45 -0400

    replaced date slashes with dashes to conform to ISO standard

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,40 +7,33 @@ st.title("""
         An exploration of rat sightings in New York City, courtesy of [NYC Open Data](https://data.cityofnewyork.us/Social-Services/Rat-Sightings/3q43-55fe).
         """)
 
-@st.cache  # add caching so we load the data only once
-def load_data():
-    filepath = "rats.csv"
-    return pd.read_csv(filepath)
+rats_url = "https://raw.githubusercontent.com/CMU-IDS-2020/a3-puppy-patrol/master/rats.csv"
 
-df = load_data()
+map = alt.Chart(rats_url).mark_circle(opacity=0.05).encode(
+        longitude="Longitude:Q",
+        latitude="Latitude:Q",
+        color="Borough:N"
+).transform_filter("datum.Longitude > -75")
+st.write(map)
 
-st.write("Let's look at raw data in the Pandas Data Frame.")
 
-#st.write(df)
-
-#chart = alt.Chart(df).mark_point().encode(
-#    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-#    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-#    color=alt.Y("species")
-#).properties(
-#    width=600, height=400
-#).interactive()
-#
-#st.write(chart)
-
-#chart = alt.Chart(df, width=800, height=600).mark_circle(size=0.1, opacity=0.25).encode(
-#        longitude="Longitude:Q",
-#        latitude="Latitude:Q",
-#        color="Address Type:N",
-#).transform_filter("datum.Longitude > -74")
-#
-#st.write(chart)
-
-bar = alt.Chart(df).mark_bar().encode(
+chart = alt.Chart(rats_url).mark_bar().encode(
         x="Borough:N",
-        y="count(City)"
+        y="count(Borough):Q"
+)
+st.write(chart)
+
+chart = alt.Chart(rats_url).mark_rect(opacity=0.05).encode(
+        x="Borough:N",
+        y="Status:N"
+)
+st.write(chart)
+
+chart = alt.Chart(rats_url).mark_line().encode(
+        x="created_date:O",
+        y="count(created_date):Q"
 )
 
-st.write(bar)
+st.write(chart)
 
-df.columns
+st.write(pd.read_csv(rats_url))
