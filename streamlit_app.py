a07e55e1507053c7de6b33603c5de313da5933ee
Lukas Hermann <lukashermann28@gmail.com>
2020-10-03 23:40:42 -0400

    added some example charts to explore

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,19 @@ st.title("""
         An exploration of rat sightings in New York City, courtesy of [NYC Open Data](https://data.cityofnewyork.us/Social-Services/Rat-Sightings/3q43-55fe).
         """)
 
+geojson_url = "https://raw.githubusercontent.com/CMU-IDS-2020/a3-puppy-patrol/master/2010_Census_Blocks.geojson" 
+geojson = alt.Data(url=geojson_url, format=alt.DataFormat(property='features', type='json'))
+
 rats_url = "https://raw.githubusercontent.com/CMU-IDS-2020/a3-puppy-patrol/master/rats.csv"
 
-map = alt.Chart(rats_url).mark_circle(opacity=0.05).encode(
+gm = alt.Chart(geojson).mark_geoshape(fill="none", stroke="black", strokeWidth=0.05)
+
+map = alt.Chart(rats_url).mark_circle(size=3.0, opacity=0.05).encode(
         longitude="Longitude:Q",
         latitude="Latitude:Q",
         color="Borough:N"
 ).transform_filter("datum.Longitude > -75")
-st.write(map)
+st.write(gm + map)
 
 
 chart = alt.Chart(rats_url).mark_bar().encode(
@@ -30,10 +35,10 @@ chart = alt.Chart(rats_url).mark_rect(opacity=0.05).encode(
 st.write(chart)
 
 chart = alt.Chart(rats_url).mark_line().encode(
-        x="created_date:T",
-        y="count(created_date):Q"
+        x=alt.X("Created Date:T",
+            timeUnit="year"),
+        y=alt.Y("Created Date:T",
+            aggregate="count",
+            timeUnit="year")
 )
-
 st.write(chart)
-
-st.write(pd.read_csv(rats_url))
