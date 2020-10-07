06e56e1a043fe8bd4742860980719d13168c62a1
Lukas Hermann <lukashermann28@gmail.com>
2020-10-07 12:29:03 -0400

    Cleaning up code a little

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,18 @@ st.title("""
         An exploration of rat sightings in New York City, courtesy of [NYC Open Data](https://data.cityofnewyork.us/Social-Services/Rat-Sightings/3q43-55fe).
         """)
 
+# Using URLs for df > 5k rows
+
 geojson_url = "https://raw.githubusercontent.com/CMU-IDS-2020/a3-puppy-patrol/master/2010_Census_Blocks.geojson" 
 geojson = alt.Data(url=geojson_url, format=alt.DataFormat(property='features', type='json'))
 
 rats_url = "https://raw.githubusercontent.com/CMU-IDS-2020/a3-puppy-patrol/master/rats.csv"
 
-gm = alt.Chart(geojson, width=800, height=800).mark_geoshape(fill="none", stroke="black", strokeWidth=0.05)
+
+gm = alt.Chart(geojson, width=800, height=800).mark_geoshape(
+        fill="none",
+        stroke="black",
+        strokeWidth=0.05)
 
 map = alt.Chart(rats_url).mark_circle(size=10, opacity=0.05).encode(
         longitude="Longitude:Q",
