0e4259001d6319a66835d0b8b1ae805545a23cf4
lukas <lukashermann28@gmail.com>
2020-10-19 03:53:24 -0400

    cleaning out unused charts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,35 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import pydeck as pdk
 from datetime import datetime
+from streamlit_observable import observable
 
-st.title(
+st.markdown(
     """
-        # Aww Rats!🐀
-        ## Who Runs New York?
+        <h1 style="font-family: serif;">Aww Rats!🐀</h1>
+        <h2 style="font-family: serif;"> Who Runs New York?</h2>
+        """,
+    unsafe_allow_html=True,
+)
+st.write(
+    """
         Rats are as synonymous with New York City as trains, the Empire State Building, and expensive apartments. There is even a [Wikipedia page dedicated to Rats in New York City](https://en.wikipedia.org/wiki/Rats_in_New_York_City). While cute and furry creatures, rats are also disease vectors (not that we need more of those right now). The City even went so far as to make a [$32 million plan](https://www.nytimes.com/2017/07/12/nyregion/new-york-city-rat-problem.html) to control the rodent problem.
         According to [NYC Open Data](https://data.cityofnewyork.us/Social-Services/Rat-Sightings/3q43-55fe) the number of reports of rat sightings has been on an upward trend.
         """
 )
 
 # Using URLs for df > 5k rows
-
 rats_url = (
     "https://raw.githubusercontent.com/CMU-IDS-2020/a3-puppy-patrol/master/rats.csv"
 )
 
 
 df = pd.read_csv("rats.csv")
-years = list(df['created_date']
-        .apply(lambda d: datetime.fromisoformat(d).year))
-min_year = min(years)
-max_year = max(years)
-select_year = st.slider("Select Year", min_year, max_year)
-
-df_seldate = (df['created_date']
-        .apply(lambda x: datetime.fromisoformat(x).year == select_year))
-st.map(df[df_seldate])
-
-boroughs = list(df['borough'].unique())
-boroughs.append('ALL')
+boroughs = list(df["borough"].unique())
+boroughs.append("ALL")
 boroughs.reverse()
 borough = st.radio("Select Borough", boroughs)
-df_borough = df if borough == "ALL" else df[df['borough'] == borough]
+df_borough = df if borough == "ALL" else df[df["borough"] == borough]
 
 chart = (
     alt.Chart(df_borough)
@@ -46,36 +40,17 @@ chart = (
     )
     .properties(width=700)
 )
+
 st.write(chart)
 
+years = list(df["created_date"].apply(lambda d: datetime.fromisoformat(d).year))
+min_year = min(years)
+max_year = max(years)
+select_year = st.slider("Select Year", min_year, max_year)
 
-
-st.write("Lets look at the number of days a request is open")
-
-chart = alt.Chart(rats_url).mark_bar().encode(x="borough:N", y="count(borough):Q")
-st.write(chart)
-
-st.write("We can also see that the data suggests Staten Island takes longer on average to close the requests.")
-
-open_mean = df.groupby(["borough"])["days_open"].mean().reset_index()
-chart = (
-    alt.Chart(open_mean)
-    .mark_bar()
-    .encode(x=alt.X("days_open:Q", title="Days Until Request Closed (Avg)"), y=alt.Y("borough:N", title="Borough"))
-    .transform_filter("datum.borough !== 'Unspecified'")
+observable(
+    key="Rat Sightings in NYC",
+    notebook="@lsh/rat-sightings-nyc",
+    targets=["map"],
+    redefine={"year": select_year},
 )
-st.write(chart)
-
-chart = (alt.Chart(rats_url)
-            .mark_rect(opacity=0.025)
-            .encode(x="borough:N", y="status:N")
-            .transform_filter('datum.created_date !== "NaN"'))
-st.write(chart)
-
-chart = (
-    alt.Chart(rats_url)
-    .mark_rect(opacity=0.25)
-    .encode(x=alt.X("created_date:T", timeUnit="hours"), y="borough:N")
-)
-
-st.write(chart)
